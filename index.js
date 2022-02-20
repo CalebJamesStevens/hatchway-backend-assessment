6920dc78abaf3c21c6c05cca6b9feca7064c2565
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-19 20:14:14 -0800

    Set up router for ping and posts

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const app = require('express')();
-const PORT = 8080;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen();
+// Directs posts api calls to posts router
+app.get('/api/posts', require('./routes/api/posts'))
+
+// Directs ping api calls to ping router
+app.get('/api/ping', require('./routes/api/ping'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
