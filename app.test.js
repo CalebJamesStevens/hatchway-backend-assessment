010f0de10764d9eba4d73b90878d7fe54c7895ae
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-19 21:49:37 -0800

    Wrote test for request with single tag param

--- app.test.js
+++ app.test.js
@@ -24,4 +24,27 @@ describe('GET /api/posts', () => {
             expect(response.body).toEqual({error: "Tags parameter is required"})
         })
     })
+
+    it('Returns array of post objects containing singular tag', () => {
+        return request(app)
+        .get('/api/posts')
+        .query({'tags':['tech']})
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then((response) => {
+            expect(response.body).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        "id": expect.any(Number),
+                        "author": expect.any(String),
+                        "authorId": expect.any(Number),
+                        "likes": expect.any(Number),
+                        "popularity": expect.any(Number),
+                        "reads": expect.any(Number),
+                        "tags": expect.any(Array) 
+                    })
+                ])
+            )
+        })
+    })
 })
