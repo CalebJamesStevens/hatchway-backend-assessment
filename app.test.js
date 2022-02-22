b29aaae095e804ee4645ef87d170f194ce26578a
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-22 13:07:02 -0800

    Created tests for soting and directions

--- app.test.js
+++ app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const _ = require('lodash');
-const app = require('./index')
+const app = require('./index');
+const { response } = require('./index');
 
 describe('GET /api/ping', () => {
     it('Returns success when pinged', () => {
@@ -15,6 +16,7 @@ describe('GET /api/ping', () => {
 })
 
 describe('GET /api/posts', () => {
+    
     it('Returns error if tags paramter is null', () => {
         return request(app)
         .get('/api/posts')
@@ -24,6 +26,93 @@ describe('GET /api/posts', () => {
         .then((response) => {
             expect(response.body).toEqual({error: "Tags parameter is required"})
         })
+    })
+
+    it('Returns error if direction paramter is invalid', () => {
+        return request(app)
+        .get('/api/posts')
+        .query({'direction': 'sdfa'})
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then((response) => {
+            expect(response.body).toEqual({error: "direction parameter is invalid"})
+        })
+    })
+
+    it('Returns error if sortBy paramter is invalid', () => {
+        return request(app)
+        .get('/api/posts')
+        .query({'sortBy': 'sdfa'})
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then((response) => {
+            expect(response.body).toEqual({error: "sortBy parameter is invalid"})
+        })
+    })
+
+    it('sorts array correctly based on sortBy parameter', async () => {
+        let originalPosts = [];
+        let sorted = true;
+
+        await request(app)
+        .get('/api/posts')
+        .query({'tags':['tech']})
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+            originalPosts = response.body.posts
+        })
+
+        const compareArrayOfObjectsByKey = (arrOne, arrTwo, key) => {
+            let temp = arrOne.sort((a,b) => {
+                return a[key] - b[key]
+            })
+
+            temp = temp.map(p => {return p[key]})
+            const responsePop = arrTwo.map(p => {return p[key]});
+
+            if (!_.isEqual(temp, responsePop)) {
+                console.log(key + ' is the issue')
+                sorted = false
+            }
+        }
+
+        const fetchSortedPosts = async (method) => {
+            await request(app)
+            .get('/api/posts')
+            .query({'tags':['tech'], 'sortBy': method})
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                compareArrayOfObjectsByKey(originalPosts, response.body.posts, method)
+            })
+        }
+
+        fetchSortedPosts('id')
+        fetchSortedPosts('reads')
+        fetchSortedPosts('likes')
+        fetchSortedPosts('popularity')
+        
+
+        return expect(sorted).toEqual(true)
+    })
+
+    it('sorts array correctly based on direction parameter', async () => {
+        const ascResponse = await request(app)
+        .get('/api/posts')
+        .query({'tags':['tech']})
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+        const descResponse = await request(app)
+        .get('/api/posts')
+        .query({'tags':['tech'], 'direction':['desc']})
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+        correctDirection = _.isEqual(descResponse.body.posts.reverse(), ascResponse.body.posts)
+ 
+        return expect(correctDirection).toEqual(true)
     })
 
     it('Returns array of post objects containing singular tag', () => {
@@ -76,9 +165,9 @@ describe('GET /api/posts', () => {
         })
     })
 
-    it('contains no post duplicates', () => {
+    it('contains no post duplicates', async () => {
         let same = false;
-        const p = request(app)
+        await request(app)
         .get('/api/posts')
         .query({'tags':['tech,history']})
         .expect('Content-Type', /json/)
@@ -86,9 +175,13 @@ describe('GET /api/posts', () => {
         .then((response) => {
             const deduped = [];
             response.body.posts.forEach(post => deduped.every(d => d.id !== post.id) && deduped.push(post))
+            console.log(deduped.length)
+            console.log(response.body.posts.length)
             same = (deduped.length == response.body.posts.length)
-            return same
+            
         })
+
+        return expect(same).toEqual(true)
 
 
         
